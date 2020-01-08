81216626fe4ba1d688e086afbd2929a40bb031fb
xif <2560343435@qq.com>
2020-01-08 22:17:15 +0800

    新增注释

--- src/main/java/com/offcn/SpringbootJpaThymeleafDemo1Application.java
+++ src/main/java/com/offcn/SpringbootJpaThymeleafDemo1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootJpaThymeleafDemo1Application {
-
+//注释------------------------
     public static void main(String[] args) {
         SpringApplication.run(SpringbootJpaThymeleafDemo1Application.class, args);
     }
